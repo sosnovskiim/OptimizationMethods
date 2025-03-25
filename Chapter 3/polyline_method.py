3d2fcf4c7b0583b1bf32ab9f6c879b2a8e728317
sosnovskiim <darkisosn2805@gmail.com>
2025-03-25 22:40:43 +0400

    Глава 3

--- Chapter 3/polyline_method.py
+++ Chapter 3/polyline_method.py
@@ -1,30 +1,29 @@
-def Lip(a, b):
-    mx = fd(a)
-    i = float(a)
-    while i < b:
-        var = fd(i)
-        if mx < var:
-            mx = var
-        i += 0.01
-    return mx
-
-
-# f = lambda x: (x + 3) * (x + 1) * x * (x - 3) - 1
 f = lambda x: x ** 4 + x ** 3 - 9 * x ** 2 - 9 * x - 1
 fd = lambda x: 4 * x ** 3 + 3 * x ** 2 - 18 * x - 9
 a, b = -3, 3
 eps = 0.00001
-L = round(Lip(a, b))
-x0 = 1 / (2 * L) * (f(a) - f(b) + L * (a + b))
-p = 1 / 2 * (f(a) + f(b) + L * (a - b))
-delta = 1 / (2 * L) * (f(x0) - p)
+L = 72
+fa, fb = f(a), f(b)
+pair_min = (
+    0.5 / L * (fa - fb + L * (a + b)),
+    0.5 * (fa + fb + L * (a - b))
+)
+pairs = [pair_min]
+x, p = pair_min[0], pair_min[1]
+fx = f(x)
+delta = 0.5 / L * (fx - p)
 while 2 * L * delta > eps:
-    x1 = x0 - delta
-    x2 = x0 + delta
-    if f(x1) < f(x2):
-        x0 = x1
-    else:
-        x0 = x2
-    p = 0.5 * (f(x0) + p)
-    delta = 1 / (2 * L) * (f(x0) - p)
-print(f"Метод ломаных: {round(x0, 5)}")
+    x1 = x - delta
+    x2 = x + delta
+    p = 0.5 * (fx + p)
+    pairs.remove(pair_min)
+    pairs.append((x1, p))
+    pairs.append((x2, p))
+    pair_min = None
+    for pair in pairs:
+        if pair_min is None or pair_min[1] > pair[1]:
+            pair_min = pair
+    x, p = pair_min[0], pair_min[1]
+    fx = f(x)
+    delta = 0.5 / L * (fx - p)
+print(f"Метод ломаных: {round(x, 5)}")
